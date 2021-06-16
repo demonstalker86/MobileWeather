de64c27b49b5ba5f0e962ed7a000144cf6c874a7
demonstalker show <demostalker26@gmail.com>
2021-06-16 16:14:30 +0300

    изменения в коде

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from pyowm.utils.config import get_default_config
 import kivy
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+
 from kivy.uix.textinput import TextInput
+
 kivy.require('1.0.7')
 
 from kivy.app import App
@@ -26,8 +28,8 @@ class TestApp(App):
          message_text = self.textinput1.text
          observation = mgr.weather_at_place(message_text)
          w = observation.weather            
-         weather_info1 = "В городе " + message_text + " сейчас " + w.detailed_status
-         weather_info2 = "Температура: " +  str(w.temperature('celsius') ['temp'])
+         weather_info1 = "В городе " + message_text + " сейчас \n" + w.detailed_status
+         weather_info2 = "Температура: " +  str(w.temperature('celsius')['temp'])
          weather_info3 = "Скорость ветра: " +  str(w.wind()['speed']) + " м/с"
          self.button1.text=weather_info1
          self.button2.text=weather_info2
@@ -41,21 +43,22 @@ class TestApp(App):
         message_text = "Тверь"
         observation = mgr.weather_at_place(message_text)
         w = observation.weather            
-        weather_info1 = "В городе " + message_text +  "\n сейчас " + w.detailed_status
-        weather_info2 = "Температура: " +  str(w.temperature('celsius') ['temp'])
+        weather_info1 = "В городе " + message_text +  " сейчас \n" + w.detailed_status
+        weather_info2 = "Температура: " +  str(w.temperature('celsius')['temp'])
         weather_info3 = "Скорость ветра: " +  str(w.wind()['speed']) + " м/с" 
         
         layout = BoxLayout(orientation="vertical")
         
         self.textinput1 = TextInput(text=message_text, multiline=False, font_size=40, halign="center")
         self.textinput1.bind(on_text_validate=self.on_enter)
-        self.button1 = Button(text=weather_info1, font_size=40)
-        self.button2 = Label(text=weather_info2, font_size=40)
-        self.button3 = Button(text=weather_info3, font_size=40)
-        self.button4 = Button(text='ПОКАЗАТЬ', font_size=40)
+
+        self.button1 = Button(text=weather_info1, font_size=30)
+        self.button2 = Label(text=weather_info2, font_size=30)
+        self.button3 = Button(text=weather_info3, font_size=30)
+        self.button4 = Button(text='ПОКАЗАТЬ', font_size=30)
         self.button4.bind(on_press=self.on_enter)
 
-        layout2 = BoxLayout(orientation="horizontal", size_hint=(1,0.5))
+        layout2 = BoxLayout(orientation="horizontal", size_hint=(1,0.4))
         layout2.add_widget(self.textinput1)
         layout2.add_widget(self.button4)
         
@@ -63,7 +66,7 @@ class TestApp(App):
         layout.add_widget(self.button1)
         layout.add_widget(self.button2)
         layout.add_widget(self.button3)
-        
+        # return a Button() as a root widget
         return  layout
 
 
